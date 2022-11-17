6438cd199938212e505df7432b8329a467559d11
xavier <xavm29@gmail.com>
2022-11-17 17:21:27 +0100

    Commit firebase and Auth Firts screnns Side menu and profile Pantalla profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 import '../styles/styles.dart';
 import '../widgets/side_menu.dart';
@@ -9,38 +10,191 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppColors.primarys,
+        backgroundColor:AppColors.primarys,
         appBar: AppBar(
-          title: Text('Profile'),
+          title: const Text("Profile"),
         ),
         drawer: const SideMenu(),
         body: Column(children: [
-          Card(
-            elevation: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage:
-                      NetworkImage('https://picsum.photos/id/237/200/300'),
-                ),
-                Text('Name'),
-              ],
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                              'https://picsum.photos/id/237/200/300'),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [Text("Marilyn Aminoff"), Text("Name")],
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text("this week"),
+                    ],
+                  ),
+                  const Divider(
+                    height: 20,
+                    thickness: 2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text("Total hours"),
+                              Text("18", style: TextStyle(fontSize: 18)),
+                            ],
+                          ),
+                        ),
+                        const Expanded(flex: 1, child: Icon(Icons.access_time)),
+                        const SizedBox(
+                            height: 45,
+                            child: VerticalDivider(
+                              thickness: 2,
+                              width: 20,
+                              color: Colors.black,
+                            )),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text("Total hours"),
+                              Text("18", style: TextStyle(fontSize: 18)),
+                            ],
+                          ),
+                        ),
+                        const Expanded(flex: 1, child: Icon(Icons.access_time)),
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    height: 20,
+                    thickness: 2,
+                    indent: 10,
+                    endIndent: 10,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                      CircularPercentIndicator(
+                        radius: 20.0,
+                        lineWidth: 5.0,
+                        percent: 1.0,
+                        progressColor: Colors.green,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
             ),
           ),
-          Row(
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text('Total horus'),
-                  Text('18'),
-                  Icon(Icons.ice_skating_outlined)
-                ],
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0, bottom: 15),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.access_time),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Total hours"),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("20 days"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           )
+          ,ElevatedButton(onPressed:( ){}, child: Text('Log out'))
         ]));
   }
+
+
 }
