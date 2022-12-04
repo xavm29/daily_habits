f1c600105e5c87b3b229902fdd7c381d2b9f0d7b
xavier <xavm29@gmail.com>
2022-12-04 21:13:12 +0100

    Commit firebase and Auth Firts screnns Side menu and profile Pantalla profile Profile update New screnss Modifications profile.dart

--- lib/screens/create_goals.dart
+++ lib/screens/create_goals.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../styles/styles.dart';
+
 class CreateGoals extends StatefulWidget {
   const CreateGoals({Key? key}) : super(key: key);
 
@@ -10,10 +12,75 @@ class CreateGoals extends StatefulWidget {
 class _CreateGoalsState extends State<CreateGoals> {
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return Scaffold(
+        backgroundColor: AppColors.primarys,
         appBar: AppBar(
           title: Text('Create Goals'),
         ),
-        body: Column());
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                  width: double.infinity,
+                  height: 320,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/Vectorfondo.png"),
+                          fit: BoxFit.fill)),
+                  child: Image.asset("assets/images/person.png")),
+              SizedBox(
+                height: 300,
+                child: Row(
+                  children: [
+                    Text("Set your Goals"),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextField(
+                      decoration: InputDecoration(
+                    labelText: 'Nº',
+                  )),
+                  TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Goal',
+                      ))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Daily"), style: style),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Wekly"), style: style),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("Montly"), style: style)
+                ],
+              ),
+              Text("Repeat everyday "),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("S"), style: style),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("M"), style: style),
+                  ElevatedButton(
+                      onPressed: () {}, child: Text("T"), style: style)
+                ],
+              ),
+              ElevatedButton(
+                  onPressed: () {}, child: Text("Continue"), style: style)
+            ],
+          ),
+        ));
   }
 }
