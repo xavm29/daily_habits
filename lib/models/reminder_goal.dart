bf999896087a340c9c00ea1267a7b52a8a6656e6
xavier <xavm29@gmail.com>
2022-12-14 09:57:48 +0100

    Commit firebase and Auth Firts screnns Side menu and profile Pantalla profile Profile update New screnss Modifications profile.dart changes on create_goals.dart changes create_goals.dart and sidemnu No funciona aquesta part reminder_goal.dart inicio Home primeros pasos

---
+++ lib/models/reminder_goal.dart
@@ -0,0 +1,10 @@
+import 'dart:core';
+import 'dart:core';
+import 'package:daily_habits/models/goals_model.dart';
+
+class ReminderGoal {
+  int repeatDaily = 0;
+  if (DateTime.now() < endDate) {
+    repeatDaily =kDaily +1 }
+
+}
